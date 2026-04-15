b6a2df50a3aeff3d9fcd9ad33bc8cbd118d02c35
Sebastian Pantzer <spantzer@middlebury.edu>
2026-04-15 14:13:32 -0400

    data test

---
+++ data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import glob
+import os
+
+# Point to your folder
+folder_path = r"C:\Users\ppant\Downloads\ML S26\RallyRF\tennis_atp"
+
+# Grab all CSV files in the folder
+csv_files = glob.glob(os.path.join(folder_path, "atp_matches_2*.csv"))
+
+# Read and concatenate them all into one DataFrame
+df = pd.concat((pd.read_csv(file) for file in csv_files), ignore_index=True)
+
+print(df.shape)
+print(df.head())
